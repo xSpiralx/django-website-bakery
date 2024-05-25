0b9c0f23f5615879a27e513d4077710c3bc5635d
Robert McDermott <robert.mcd@live.com>
2024-05-25 09:58:22 -0400

    Base Update
    This adds all the basic stuff I need to get this website rolling!

--- robs_rolls/bakery/models.py
+++ robs_rolls/bakery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
+
+class WeeklyTreat(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    week_start = models.DateField()
+    week_end = models.DateField()
+
+    def __str__(self):
+        return f"{self.product.name} ({self.week_start} - {self.week_end})"
--- robs_rolls/bakery/views.py
+++ robs_rolls/bakery/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product, WeeklyTreat
 
-# Create your views here.
+def home(request):
+    return render(request, 'bakery/home.html')
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'bakery/products.html', {'products': products})
+
+def weekly_treats(request):
+    weekly_treats = WeeklyTreat.objects.all()
+    return render(request, 'bakery/weekly_treats.html', {'weekly_treats': weekly_treats})
